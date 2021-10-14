0d69fdb7de49533038f9e872d38edc02bc48a09e
akhilsingh2003 <akhilsinghrajpoot1@gmail.com>
2021-10-14 17:15:23 +0530

    custom class creation

---
+++ JAVA/src/custom_class.java
@@ -0,0 +1,15 @@
+class empolyee
+{
+    int id;
+    String name;
+}
+public class custom_class {
+    public static void main(String[] args) {
+        System.out.println("this is our custom class");
+        empolyee ak =new empolyee();
+        ak.id=12;
+        ak.name="Akhil";
+        System.out.println(ak.id);
+        System.out.println(ak.name);
+    }
+}
