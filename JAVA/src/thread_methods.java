75166bff2edde0e5adce1d051bdedff367580db4
akhilsingh2003 <akhilsinghrajpoot1@gmail.com>
2021-11-26 19:31:35 +0530

    thread methods

---
+++ JAVA/src/thread_methods.java
@@ -0,0 +1,91 @@
+
+
+
+
+//The methods which are used in this program are following
+
+//start()
+//run()
+//setName()
+//getName()
+// SetPriority()
+//SetDaemon()
+//yield()
+
+class GuessANumber extends Thread {
+    private int number;
+    public GuessANumber(int number) {
+        this.number = number;
+    }
+
+    public void run() {
+        int counter = 0;
+        int guess = 0;
+        do {
+            guess = (int) (Math.random() * 100 + 1);
+            System.out.println(this.getName() + " guesses " + guess);
+            counter++;
+        } while(guess != number);
+        System.out.println("** Correct!" + this.getName() + "in" + counter + "guesses.**");
+    }
+}
+class DisplayMessage implements Runnable {
+    private String message;
+
+    public DisplayMessage(String message) {
+        this.message = message;
+    }
+
+    public void run() {
+        while(true) {
+            System.out.println(message);
+        }
+    }
+}
+class MyThread extends Thread {
+    public void run() {
+        for (int i = 0; i < 5; ++i) {
+            Thread.yield();
+            System.out.println("Thread started:" + Thread.currentThread().getName());
+        }
+        System.out.println("Thread ended:" + Thread.currentThread().getName());
+    }
+}
+public class thread_methods {
+
+    public static void main(String [] args) {
+        Runnable hello = new DisplayMessage("Hello");
+        Thread thread1 = new Thread(hello);
+        thread1.setDaemon(true);
+        thread1.setName("hello");
+        System.out.println("Starting hello thread...");
+        thread1.start();
+
+        Runnable bye = new DisplayMessage("Goodbye");
+        Thread thread2 = new Thread(bye);
+        thread2.setPriority(Thread.MIN_PRIORITY);
+        thread2.setDaemon(true);
+        System.out.println("Starting goodbye thread...");
+        thread2.start();
+
+        System.out.println("Starting thread3...");
+        Thread thread3 = new GuessANumber(27);
+        thread3.start();
+        try {
+            thread3.join();
+        } catch (InterruptedException e) {
+            System.out.println("Thread interrupted.");
+        }
+        System.out.println("Starting thread4...");
+        Thread thread4 = new GuessANumber(75);
+
+        thread4.start();
+        System.out.println("main() is ending...");
+        MyThread thread = new MyThread();
+        thread.start();
+        for (int i = 0; i < 5; ++i) {
+            System.out.println("Thread started:" + Thread.currentThread().getName());
+        }
+        System.out.println("Thread ended:" + Thread.currentThread().getName());
+    }
+    }
